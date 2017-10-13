4661ff16001405a7f628d0edbe01195d244bb645
Yoel Gluschnaider <yoel.gluschnaider@skyscanner.net>
2017-10-13 12:05:34 +0100

    Update the version of the build tools and gradle
    Also removed some unused code and added .gradle to the .gitignore

--- android/src/main/java/com/smixx/fabric/FabricPackage.java
+++ android/src/main/java/com/smixx/fabric/FabricPackage.java
@@ -1,7 +1,6 @@
 package com.smixx.fabric;
 
 import com.facebook.react.ReactPackage;
-import com.facebook.react.bridge.JavaScriptModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
@@ -18,11 +17,6 @@ public class FabricPackage implements ReactPackage {
         modules.add(new SMXCrashlytics(reactContext));
         modules.add(new SMXAnswers(reactContext));
         return modules;
-    }
-
-    // Deprecated RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return new ArrayList<>();
     }
 
     @Override
